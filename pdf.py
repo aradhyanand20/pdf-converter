b5cd9688c181d9557b3f9d19eb32398488c0a4ea
aradhyanand20 <aradhyanand24k@gmail.com>
2025-10-19 01:38:53 +0530

    create lined pages in pdf

--- pdf.py
+++ pdf.py
@@ -12,8 +12,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln =1)
+    for y in range(20,277,10):
+       pdf.line(20, y, 200, y)
 # ln - is for line break
-    pdf.line(10,21,200,22)
+#     pdf.line(10,21,200,22)
 
     #Set the footer
     pdf.ln(265)
@@ -27,5 +29,8 @@ for index, row in df.iterrows():
        pdf.set_font(family = "Times", style="I", size= 8)
        pdf.set_text_color(100,180,180)
        pdf.cell(w=0, h=0, txt=row["Topic"],align ="R")
+       for y in range(20, 277, 10):
+           pdf.line(20, y, 200, y)
 
+       pdf.ln(26)
 pdf.output("output.pdf")
